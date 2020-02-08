3afa1b811a43385c8a1b8f745ee88b3adb720ccb
Oleg Silkin <o.silkin98@gmail.com>
2020-02-08 13:26:00 -0500

    Adds sources

---
+++ src/transactions/Transaction.java
@@ -0,0 +1,32 @@
+package transactions;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/* TODO: create CSV reader class
+ *   TODO: add essential field specification for csv parser
+ *    TODO: find hash function that produces a digest size & convert from hex to base32 so digest is 32 digits
+ *     TODO: add linter */
+
+public class Transaction {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader csvReader = new BufferedReader(new FileReader("customers.csv"));
+        // set -1 so it captures all empty strings
+        String[] schema = csvReader.readLine().split(",", -1);
+        for (var title: schema) {
+            System.out.print(title + ", ");
+        }
+
+        List<String[]> dataRows = new ArrayList<String[]>();
+        String row;
+        while((row = csvReader.readLine()) != null) {
+            // catch the empty strings
+            dataRows.add(row.split(",", -1));
+        }
+        csvReader.close();
+    }
+}
