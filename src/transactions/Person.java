37883472fce7d2d457ff9d235c3926d2dcfe1017
Kenneth Tubman V <tubofpower@gmail.com>
2020-02-15 17:28:17 -0500

    Switched to using Java conventions of camelCase for variables

--- src/transactions/Person.java
+++ src/transactions/Person.java
@@ -3,120 +3,89 @@ package transactions;
 import java.util.Map;
 import java.util.UUID;
 
+/**
+ * A Person is an abstraction for a bunch of details containing a Person
+ */
 public class Person {
-    private String AccountName, Company,FirstName,LastName,Address_1,Address_2,City,State,Zip,TransactionID;
+    private String accountName, company, firstName, lastName, address1, address2, city, state, zip;
 
-    public Person(String AccountName, String Company, String FirstName, String LastName, String Address_1, String Address_2, String City, String State, String Zip){
-        this.AccountName = AccountName;
-        this.Company = Company;
-        this.FirstName = FirstName;
-        this.LastName = LastName;
-        this.Address_1 = Address_1;
-        this.Address_2 = Address_2;
-        this.City = City;
-        this.State = State;
-        this.Zip = Zip;
-        this.TransactionID = generateId();
+    public Person(String accountName, String company, String firstName, String lastName, String address1, String address2, String city, String state, String zip) {
+        this.accountName = accountName;
+        this.company = company;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address1 = address1;
+        this.address2 = address2;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
     }
 
     public Person(String[] paramList) {
-        this.AccountName = paramList[0];
-        this.Company = paramList[1];
-        this.FirstName = paramList[2];
-        this.LastName = paramList[3];
-        this.Address_1 = paramList[4];
-        this.Address_2 = paramList[5];
-        this.City = paramList[6];
-        this.State = paramList[7];
-        this.Zip = paramList[8];
-        this.TransactionID = generateId();
+        this.accountName = paramList[0];
+        this.company = paramList[1];
+        this.firstName = paramList[2];
+        this.lastName = paramList[3];
+        this.address1 = paramList[4];
+        this.address2 = paramList[5];
+        this.city = paramList[6];
+        this.state = paramList[7];
+        this.zip = paramList[8];
     }
 
     public Person(Map<String, String> props) {
-        this.AccountName = props.getOrDefault("Account", "");
-        this.Company = props.getOrDefault("Company", "");
-        this.FirstName = props.getOrDefault("FirstName", "");
-        this.LastName = props.getOrDefault("LastName", "");
-        this.Address_1 = props.getOrDefault("Address_1", "");
+        this.accountName = props.getOrDefault("Account", "");
+        this.company = props.getOrDefault("Company", "");
+        this.firstName = props.getOrDefault("FirstName", "");
+        this.lastName = props.getOrDefault("LastName", "");
+        this.address1 = props.getOrDefault("Address_1", "");
     }
 
     public String getAccountName(){
-        return AccountName;
-    }
-
-    public void setAccountName(String accountName){
-        AccountName = accountName;
+        return accountName;
     }
 
     public String getCompany(){
-        return Company;
-    }
-
-    public void setCompany(String company){
-        Company = company;
+        return company;
     }
 
     public String getFirstName(){
-        return FirstName;
-    }
-
-    public void setFirstName(String firstName){
-        FirstName = firstName;
+        return firstName;
     }
 
     public String getLastName(){
-        return LastName;
-    }
-
-    public void setLastName(String lastName){
-        LastName = lastName;
+        return lastName;
     }
 
     public String getAddress_1(){
-        return Address_1;
-    }
-
-    public void setAddress_1(String address_1){
-        Address_1 = address_1;
+        return address1;
     }
 
     public String getAddress_2(){
-        return Address_2;
-    }
-
-    public void setAddress_2(String address2){
-        Address_2 = address2;
+        return address2;
     }
 
     public String getCity(){
-        return City;
-    }
-
-    public void setCity(String city){
-        City = city;
+        return city;
     }
 
     public String getState(){
-        return State;
-    }
-
-    public void setState(String state){
-        State = state;
+        return state;
     }
 
     public String getZip(){
-        return Zip;
+        return zip;
     }
 
-    public void setZip(String zip){
-        Zip = zip;
+    public String getTransactionId() {
+        return this.generateId();
     }
 
     /**
      * Generate's a unique 24 digit alphanumeric transaction ID for this Person
      * @return String which is a 24 digit alphanumeric transaction ID
      */
-    private static String generateId(){
+    private String generateId(){
         UUID uuid = UUID.randomUUID();
         String tranId = uuid.toString();
         tranId = tranId.replaceAll("\\-","");
@@ -126,16 +95,16 @@ public class Person {
 
     @Override
     public String toString(){
-        return ("Account: " + AccountName + "\n" +
-                    "Company: " + Company + "\n" +
-                    "First Name: " + FirstName + "\n" +
-                    "Last Name: " + LastName + "\n" +
-                    "Address 1: " + Address_1 + "\n" +
-                    "Address 2: " + Address_2 + "\n" +
-                    "City: " + City + "\n" +
-                    "State: " + State + "\n" +
-                    "Zip: " + Zip + "\n" +
-                    "TransactionID: " + TransactionID + "\n"
+        return ("Account: " + this.accountName + "\n" +
+                    "Company: " + this.company + "\n" +
+                    "First Name: " + this.firstName + "\n" +
+                    "Last Name: " + this.lastName + "\n" +
+                    "Address 1: " + this.address1 + "\n" +
+                    "Address 2: " + this.address2 + "\n" +
+                    "City: " + this.city + "\n" +
+                    "State: " + this.state + "\n" +
+                    "Zip: " + this.zip + "\n" +
+                    "Transaction ID: " + this.getTransactionId() + "\n"
         );
     }
 }
