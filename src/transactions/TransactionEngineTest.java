922454e8307ed42fed8e71cb9a5a627b7bbc9555
Emma Gertje <egertje@wpi.edu>
2020-04-08 17:44:48 -0400

    Finished JUnit tests for DataManager and TransactionEngine

--- src/transactions/TransactionEngineTest.java
+++ src/transactions/TransactionEngineTest.java
@@ -8,7 +8,9 @@ import static org.junit.Assert.*;
 /**
  * TransactionEngineTest is used to test both the generateID() method in the Person
  * class to ensure that a random 24 alpha numeric transaction ID is created for each person
- * and also the getSchema() and getPersonMap() methods in the DataManager class
+ * and also the createPersonMap() method in the DataManager class
+ * TransactionEngineTest also tests the methods in TransactionEngine to make sure they work correctly
+ * by making sure that the transaction IDs are created for the given file or input
  */
 public class TransactionEngineTest {
     @Test
@@ -29,6 +31,7 @@ public class TransactionEngineTest {
         //here I test the generateID() method for the Person objects I've created by calling the
         //getTransactionId() method which calls generateID() to make sure transaction IDs are created for people
         //with null, empty strings, and white space for different fields
+        //I am doing assertNotNull to make sure that the transaction IDs are actually created
         assertNotNull(person1.getTransactionId());
         assertNotNull(person2.getTransactionId());
         assertNotNull(person3.getTransactionId());
@@ -43,6 +46,7 @@ public class TransactionEngineTest {
             //transaction IDs are all 24 digits, are alpha numeric, and are all unique
         String[] transactionIDs = new String[1000];
         for(int i = 0; i < transactionIDs.length; i++) {
+            //we recreated this Person object inside the for loop so a new transaction ID would be generated each time
             Person person2 = new Person("0000000011", "Hogwarts", "Ron", "Weesley", "4 Wizard Way", null, "London", null, null);
             transactionIDs[i] = person2.getTransactionId();
         }
@@ -55,5 +59,50 @@ public class TransactionEngineTest {
             }
         }
     }
+    //Now I will test the createPersonMap() methods in the DataManager class by creating a csv file
+        //with a few Person objects in it then create a DataManager class and make sure that a Person map is created
+        //for Person objects that have null fields (such as address) and that a transaction ID is created for the Person
+        //objects in the csv file
+    @Test
+    public void testIDCreatedWithGetPersonMap() {
+        DataManager dataManager = new DataManager("Test-Customers.csv");
+        //now do the createPersonMap() method to create a map of Person objects with their account number
+            //as their key
+        Map<String, Person> personMap = dataManager.createPersonMap();
+        //Now I will get a few of the Person objects in the csv file and store them here to see if there are
+            //transaction IDs for them
+        Person person1 = personMap.get("abc123");
+        Person person2 = personMap.get("success789");
+        Person person3 = personMap.get("16554846");
+        //person1 has several empty fields in the CSV file so we want to make sure a transaction ID is created
+        assertNotNull(person1.getTransactionId());
+        //person2 also has an empty field in the CSV file
+        assertNotNull(person2.getTransactionId());
+        //person3 has no empty fields in the CSV file
+        assertNotNull(person3.getTransactionId());
+    }
+    //Now I will test the getAllCustomerTransactionIDs and getCustomerTransactionID methods
+    //To do this I created a csv file with a few Person objects stored in it and am using this csv file to create
+        //a DataManager object to test these methods
+    @Test
+    public void testGetAllCustomerTransactionIDs() {
+        TransactionEngine transactions = new TransactionEngine("Test-Customers.csv");
+        //to test getAllCustomerTransactionIDs I will make sure the output of this method isn't null
+        assertNotNull(transactions.getAllCustomerTransactionIDs());
+        //Now I will make sure there are 5 transaction IDs in the list created by getAllCustomerTransactionIDs
+            //since there's 5 customers in Test-Customers
+        assertEquals(5, transactions.getAllCustomerTransactionIDs().size());
+    }
+    @Test
+    public void testGetCustomerTransactionID() {
+        TransactionEngine transactions = new TransactionEngine("Test-Customers.csv");
+        //To test this I will make sure the output of getCustomerTransactionID is not null for a few of the
+            //customers' account numbers which shows that the transaction IDs were created for those customers
+            //and that it gets the transaction ID for that account number
+        assertNotNull(transactions.getCustomerTransactionID("abc123"));
+        assertNotNull(transactions.getCustomerTransactionID("success789"));
+        assertNotNull(transactions.getCustomerTransactionID("147846894"));
+    }
+
 }
 
